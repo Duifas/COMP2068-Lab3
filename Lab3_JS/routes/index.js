5853762210e05ecc81892000f5877a69ab333e30
Thales Valente <46799430+Duifas@users.noreply.github.com>
2020-06-15 15:17:26 -0400

    Final Changes

--- Lab3_JS/routes/index.js
+++ Lab3_JS/routes/index.js
@@ -7,4 +7,28 @@ router.get('/', function (req, res) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/thales', function (req, res) {
+    res.render('thales', { description: ' is a student from Georgian College. He has been living in Canada for about 3 years now. He enjoys programming and reading books.' });
+});
+
+router.get('/monika', function (req, res) {
+    res.render('monika', { description: " is Thales' mother. She is currently working in Belgium and learning Dutch to expand her vocabulary." });
+});
+
+router.get('/cristiano', function (req, res) {
+    res.render('cristiano', { description: " is Thales' father. He is currently living and Brazil and enjoys cooking different meals everyday." });
+});
+
+router.get('/leandro', function (req, res) {
+    res.render('leandro', { description: " is Thales' stepfather. He is living in Belgium along with his wife and son. He enjoys watching soccer games and making jokes." });
+});
+
+router.get('/joao', function (req, res) {
+    res.render('joao', { description: " is Thales' little half-brother. He is studying Dutch in Belgium and enjoys spending time with his dog." });
+});
+
+router.get('/duda', function (req, res) {
+    res.render('duda', { description: " is Thales' sister. She is studyin psychology in Brazil and likes to listen to music. She also has a weird fashion sense." });
+});
+
 module.exports = router;
